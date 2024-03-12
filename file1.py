27213da8c0a615098496730e927f78cb46b75a70
Madhu-034 <163089162+Madhu-034@users.noreply.github.com>
2024-03-12 15:32:05 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,68 @@
+print("hello world")
+'''
+day1
+ what kiund of language is python
+---> python is a
+ 1.)object orinted programing language
+ 2.)dynamically typed language
+ 3.)interpreted language
+ 4.)functional programing language
+----->commenting
+1.)commenting is used for mking doccumentation in a python file
+2.)when the line commented, that line will not be considered as a part part of the code
+3.)when the program is executed line will not be part of execution
+
+----->NOTE
+single line commenting can be done using # at the beginig of the line
+    eg:
+        #print("hello 1")
+---->bargained characters
+controls and data structuirs->ABC language
+string and its formate->from C
+regular expressions ->pearl
+exception handling ->module 3
+magic methods from ->fortron
+-->flavours of python
+*Cpython-written in C,most common implementation of python.
+*Jython-java implementation of python.
+*IronPython-implementation of the python programming language.
+*Brython-browser python, runs in browsers.
+*rubypython-bridge between python and ruby interpreters.
+--->print(object, sep, end, file, flush)
+print has 5 perameters
+
+object -->the value to be printed is object
+sep-->seperator
+eg:
+a = 8
+b = 9
+c = 89
+print(a, b, c,  sep='$')
+
+end-->which is used to add the string at the end of the value,
+It will not keep the command to stay in same line
+eg:
+print (str(a),b, end='%%')
+print(str(c))
+
+! ---> variables
+?variable is a name which is used to identifuy the value of variable
+?which is stored in a spesific memory location
+
+!rules to define variables
+1.)variable have to start with only alphabets, underscores
+2.)the charector following the 1st char can be alphabets,numbers, underscores
+3.)no space are allowed
+4.)no spl charectors like !@#$%%^^&()=
+5.)case sensitive
+6.)keywords are not allowed as variables
+
+'''
+import this
+print(this)
+'''
+import is a key which is used to communicated between the python files
+'''
+import antigravity
+
+
